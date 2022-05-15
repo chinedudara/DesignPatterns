9a2fb8368d40a3d2d706f17561f06649ad03aae2
Chinedu Okechukwu-Dara <darachinedu@gmail.com>
2022-05-15 09:29:29 +0100

    Added Abstract Factory Pattern

---
+++ DesignPatterns/CreationalDesignPatterns/AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalDesignPatterns.AbstractFactory
+{
+    #region Door
+    public interface Door
+    {
+        void GetDescription();
+    }
+
+    public class WoodenDoor : Door
+    {
+        public void GetDescription()
+        {
+            Console.WriteLine("I am a wooden door");
+        }
+    }
+
+    public class IronDoor : Door
+    {
+        public void GetDescription()
+        {
+            Console.WriteLine("I am an iron door");
+        }
+    }
+    #endregion
+
+    #region FittingExpert
+    public interface DoorFittingExpert
+    {
+        void GetDescription();
+    }
+
+    public class Carpenter : DoorFittingExpert
+    {
+        public void GetDescription()
+        {
+            Console.WriteLine("I fit wooden doors only");
+        }
+    }
+
+    public class Welder : DoorFittingExpert
+    {
+        public void GetDescription()
+        {
+            Console.WriteLine("I fit iron doors only");
+        }
+    }
+    #endregion
+
+
+    //-------------------------------------//
+    //----------- Factory Class -----------//
+    //-------------------------------------//
+    public interface DoorFactory
+    {
+        Door MakeDoor();
+        DoorFittingExpert MakeFittingExpert();
+    }
+
+    public class WoodenDoorFactory : DoorFactory
+    {
+        public Door MakeDoor() => new WoodenDoor();
+
+        public DoorFittingExpert MakeFittingExpert() => new Carpenter();
+    }
+
+    public class IronDoorFactory : DoorFactory
+    {
+        public Door MakeDoor() => new IronDoor();
+
+        public DoorFittingExpert MakeFittingExpert() => new Welder();
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,20 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DesignPatterns.CreationalDesignPatterns.AbstractFactory;
 using DesignPatterns.CreationalDesignPatterns.FactoryMethod;
-using DesignPatterns.CreationalDesignPatterns.SimpleFactory;
+using DoorFactory = DesignPatterns.CreationalDesignPatterns.SimpleFactory.DoorFactory;
 
 Console.WriteLine("Creational Design Patterns");
 Console.WriteLine("----------------------------------");
 Console.WriteLine(Environment.NewLine);
 
+#region Simple Factory
+{
+    Console.WriteLine("Simple Factory");
+    var door = DoorFactory.MakeDoor(20, 10);
+    Console.WriteLine($"Width: {door.GetHeight()}");
+    Console.WriteLine($"Height: {door.GetWidth()}");
+    Console.WriteLine(Environment.NewLine);
+}
+#endregion
 
-Console.WriteLine("Simple Factory");
-var door = DoorFactory.MakeDoor(20, 10);
-Console.WriteLine($"Width: {door.GetHeight()}");
-Console.WriteLine($"Height: {door.GetWidth()}");
-Console.WriteLine(Environment.NewLine);
+#region Factory Method
+{
+    Console.WriteLine("Factory Method");
+    var marketingManager = new MarketingManager();
+    marketingManager.TakeInterview();
+    var devManager = new DevelopmentManager();
+    devManager.TakeInterview();
+    Console.WriteLine(Environment.NewLine);
+}
 
-Console.WriteLine("Factory Method");
-var interviewer = new MarketingManager();
-interviewer.TakeInterview();
-var interviewer2 = new DevelopmentManager();
-interviewer2.TakeInterview();
+#endregion
+
+#region Abstract Factory
+{
+    Console.WriteLine("Abstract Factory");
+    var woodenDoorFactory = new WoodenDoorFactory();
+    var door = woodenDoorFactory.MakeDoor();
+    var fitter = woodenDoorFactory.MakeFittingExpert();
+    door.GetDescription();
+    fitter.GetDescription();
+
+    var ironDoorFactory = new IronDoorFactory();
+    door = ironDoorFactory.MakeDoor();
+    fitter = ironDoorFactory.MakeFittingExpert();
+    door.GetDescription();
+    fitter.GetDescription();
+    Console.WriteLine(Environment.NewLine);
+}
+
+#endregion
+
